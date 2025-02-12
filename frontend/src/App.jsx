55d5f675dc07053a5305daf020d71953f30f1967
Mayank Roy <mayankroy106106@gmail.com>
2025-02-12 15:20:00 +0530

    feat: enhance Navbar layout and structure in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import './index.css'
-import Navbar from './assets/components/Navbar'
+import { useState } from "react";
+import "./index.css";
+import Navbar from "./assets/components/Navbar";
 
 function App() {
-  
-
   return (
     <>
-     <div className='bg-black h-screen w-full'>
-        <Navbar />
-     </div>
+      <div className="bg-black h-screen w-full">
+        <div className="grid grid-flow-row grid-cols-[300px_auto_350px] h-full ">
+          <Navbar />
+          
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/assets/components/Navbar.jsx
+++ frontend/src/assets/components/Navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div className="grid grid-flow-row grid-cols-[300px_auto_350px] h-full ">
-          <div className=' p-3 navbar text-white' >
-            <h1 className='text-4xl font-bold '>DevConnect</h1>
-            <div className='flex items-center justify-center mt-20'>
-              <ul className='text-4xl '>
-                <li className='m-4 hover:cursor-pointer'>Home</li>
-                <li className='m-4 hover:cursor-pointer'>Explore</li>
-                <li className='m-4 hover:cursor-pointer'>Notification</li>
-                <li className='m-4 hover:cursor-pointer'>More...</li>
-              </ul>
-            </div>
-            <button className='text-4xl text-black font-bold px-15 py-2  mt-12 ml-7 border-2 rounded-4xl hover:cursor-pointer bg-[#1d9bf0]'>Post</button>
-          </div>
-          <div className='bg-green-400 '></div>
-          <div className='bg-red-400  '></div>
+    <>
+      <div className=" p-3 navbar text-white">
+        <h1 className="text-4xl font-bold ">DevConnect</h1>
+        <div className="flex items-center justify-center mt-20">
+          <ul className="text-4xl ">
+            <li className="m-4 hover:cursor-pointer">Home</li>
+            <li className="m-4 hover:cursor-pointer">Explore</li>
+            <li className="m-4 hover:cursor-pointer">Notification</li>
+            <li className="m-4 hover:cursor-pointer">More...</li>
+          </ul>
         </div>
-  )
+        <button className="text-4xl text-black font-bold px-15 py-2  mt-12 ml-7 border-2 rounded-4xl hover:cursor-pointer bg-[#1d9bf0]">
+          Post
+        </button>
+      </div>
+      <div className="bg-green-400 main"></div>
+      <div className="filler text-white  ">
+        <div>
+          <h2 className="text-4xl font-bold m-2 ">Featured Devs</h2>
+          <div className="flex flex-col text-2xl my-10 mx-5 ">
+            <span className="p-2">Developer</span>
+            <span className="p-2">Developer</span>
+            <span className="p-2">Developer</span>
+            <span className="p-2">Developer</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;

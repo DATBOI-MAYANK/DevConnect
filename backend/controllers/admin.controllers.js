cbf0a0015e5140f71fa972cdacd99d575d79e830
Mayank Roy <mayankroy106106@gmail.com>
2025-03-17 19:06:35 +0530

    feat: implement admin login functionality and update registration to use Adminname

--- backend/controllers/admin.controllers.js
+++ backend/controllers/admin.controllers.js
@@ -7,21 +7,21 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const registerAdmin = asyncHandler(async (req, res) => {
   // get admin details from frontend
 
-  const { username, password, email } = req.body;
+  const { Adminname, password, email } = req.body;
 
   //Validation -- Not empty
 
-  if ([username, email, password].some((fields) => fields.trim() === "")) {
+  if ([Adminname, email, password].some((fields) => fields.trim() === "")) {
     throw new ApiError(400, "All Fields are required !!");
   }
   if (!email.trim().toLowerCase().endsWith("@gmail.com")) {
     throw ApiError(400, "Ivalid Email");
   }
 
-  // check if user already exits: username,email
+  // check if Admin already exits: Adminname,email
 
   const existedAdmin = await Admin.findOne({
-    $or: [{ username }, { email }],
+    $or: [{ Adminname }, { email }],
   });
 
   if (existedAdmin) {
@@ -46,10 +46,10 @@ const registerAdmin = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Avatar File is required!");
   }
 
-  // create user object -- create entry in db
+  // create Admin object -- create entry in db
 
   const admin = await Admin.create({
-    username: username.toLowerCase(),
+    Adminname: Adminname.toLowerCase(),
     AvatarImage: avatar.url,
     coverImage: coverImage?.url || "",
     email,
@@ -78,4 +78,49 @@ const registerAdmin = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdAdmin, "Admin registered successfully"));
 });
 
-export { registerAdmin };
+const loginAdmin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    throw new ApiError(400, "All Fields are required. !!");
+  }
+
+  const admin = await Admin.findOne({ email });
+
+  if (!admin) {
+    throw new ApiError(404, "Admin does not exist. !");
+  }
+
+  const isPasswordValid = await admin.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Passoword incorrect. !");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    admin._id
+  );
+
+  const loggedInAdmin = await Admin.findById(admin._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("Access Token", accessToken, options)
+    .cookie("Refresh Token", refreshToken, options)
+    .json(200, {
+      admin: accessToken,
+      loggedInAdmin,
+      refreshToken,
+    },
+    "Admin logged in succesfully."
+  );
+});
+
+export { registerAdmin, loginAdmin };

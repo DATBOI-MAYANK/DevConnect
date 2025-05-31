eba3b80b45179e3e48545c79bbff50e2493d043a
Mayank Roy <mayankroy106106@gmail.com>
2025-05-31 13:42:22 +0530

    feat: integrate ClickSpark component into Login and SignUp forms, add username field to SignUp

--- frontend/src/assets/components/LogIn.jsx
+++ frontend/src/assets/components/LogIn.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import ClickSpark from "./ClickSpark";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -23,34 +24,48 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex  justify-center">
-      <form onSubmit={handleLogin} className="bg-[#171717] flex flex-col p-5 mt-40 rounded-md">
-        <h2 className="text-4xl text-white text-center p-2">Login Form</h2>
-        <label htmlFor="email" className="text-white ml-4 mt-2">Email</label>
-        <input
-          className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
-          type="email"
-          value={email}
-          placeholder="me@example.com"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password" className="text-white ml-4">Password</label>
-        <input
-          className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="text-4xl text-black font-bold   mt-2  border-1 rounded-md hover:cursor-pointer bg-[#1d9bf0] hover:bg-[#48CAE4]" type="submit">Login</button>
-         <div className="mt-4 text-center text-white text-sm">
-              Don&apos;t have an account?{" "}
-              <Link to="/signup" className="underline underline-offset-4">
-                Sign up
-              </Link>
-            </div>
-      </form>
-    </div>
+    <ClickSpark>
+      <div className="flex  justify-center">
+        <form
+          onSubmit={handleLogin}
+          className="bg-[#171717] flex flex-col p-5 mt-40 rounded-md"
+        >
+          <h2 className="text-4xl text-white text-center p-2">Login </h2>
+          <label htmlFor="email" className="text-white ml-4 mt-2">
+            Email
+          </label>
+          <input
+            className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
+            type="email"
+            value={email}
+            placeholder="me@example.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password" className="text-white ml-4">
+            Password
+          </label>
+          <input
+            className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="text-4xl text-black font-bold   mt-2  border-1 rounded-md hover:cursor-pointer bg-[#1d9bf0] hover:bg-[#48CAE4]"
+            type="submit"
+          >
+            Login
+          </button>
+          <div className="mt-4 text-center text-white text-sm">
+            Don&apos;t have an account?{" "}
+            <Link to="/signup" className="underline underline-offset-4">
+              Sign up
+            </Link>
+          </div>
+        </form>
+      </div>
+    </ClickSpark>
   );
 };
 
--- frontend/src/assets/components/SignUp.jsx
+++ frontend/src/assets/components/SignUp.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import ClickSpark from "./ClickSpark";
 const SignUp = () => {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -9,7 +11,7 @@ const SignUp = () => {
     try {
       const res = await axios.post(
         "http://localhost:8000/users/api/v1/SignUp",
-        { email, password },
+        {username, email, password },
         { withCredentials: true }
       );
 
@@ -21,35 +23,53 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex  justify-center">
-      <form onSubmit={handleSignUp} className="bg-[#171717] flex flex-col p-5 mt-40 rounded-md">
-        <h2 className="text-4xl text-white text-center p-2">SignUp Form</h2>
-        <label htmlFor="email" className="text-white ml-4 mt-2">Email</label>
-        <input
-          className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
-          type="email"
-          value={email}
-          placeholder="me@example.com"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password" className="text-white ml-4">Password</label>
-        <input
-          className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="text-4xl text-black font-bold   mt-2  border-1 rounded-md hover:cursor-pointer bg-[#1d9bf0] hover:bg-[#48CAE4]" type="submit">SignUp</button>
-         <div className="mt-4 text-center text-white text-sm">
-              Don&apos;t have an account?{" "}
-              <Link to="/signup" className="underline underline-offset-4">
-                Sign up
-              </Link>
-            </div>
-      </form>
-    </div>
+    <ClickSpark>
+      <div className="flex  justify-center">
+        <form
+          onSubmit={handleSignUp}
+          className="bg-[#171717] flex flex-col p-5 mt-40 rounded-md"
+        >
+          <h2 className="text-4xl text-white text-center p-2">SignUp</h2>
+          <label htmlFor="username" className="text-white ml-4 mt-2">
+            Username
+          </label>
+          <input
+            className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
+            type="username"
+            value={username}
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="email" className="text-white ml-4 mt-2">
+            Email
+          </label>
+          <input
+            className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
+            type="email"
+            value={email}
+            placeholder="me@example.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password" className="text-white ml-4">
+            Password
+          </label>
+          <input
+            className="w-80 h-9 bg-white rounded-xs mx-4 mb-4 p-2"
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="text-4xl text-black font-bold   mt-2  border-1 rounded-md hover:cursor-pointer bg-[#1d9bf0] hover:bg-[#48CAE4]"
+            type="submit"
+          >
+            SignUp
+          </button>
+        </form>
+      </div>
+    </ClickSpark>
   );
 };
 
-export default SignUp
+export default SignUp;
